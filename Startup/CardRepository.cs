65f78c07d59a579311eba5ac7471c316ab6ec661
Matthæus <Matthus@MATTHUSPC>
2022-09-17 21:15:44 +0200

    Add project files.

---
+++ Startup/CardRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Globalization;
+
+namespace Startup
+{
+    public class CardRepository
+    {
+            public string[] cards = { "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "P", "Q", "K", "JOKER"};
+            public string[] colors = { "Hearts", "Spades", "Clubs", "Diamonds" };
+    }
+}
---
+++ Startup/Controllers/CardgameController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Startup.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CardgameController : Controller
+    {
+        [HttpGet(Name = "SelectRandomCard")]
+        public IActionResult Get()
+        {
+            CardRepository cardRepository = new CardRepository();
+            String card;
+            String color;
+            String result;
+            Random rand = new Random();
+            string json = "";
+            int index = rand.Next(cardRepository.cards.Length);
+
+            card = cardRepository.cards[index];
+            if (card != "JOKER")
+            {
+                index = rand.Next(cardRepository.colors.Length);
+                color = cardRepository.colors[index];
+                result = card + " Of "+ color;
+                json = JsonConvert.SerializeObject(result);
+
+
+            }
+            else
+            {
+                json = JsonConvert.SerializeObject(card);
+
+            }
+            return Ok(json) ;
+        }
+    }
+}
